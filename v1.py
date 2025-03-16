4f33185ff6834d5cb1a7dcc85dbc258e7a3330a0
Ethan Silverthorne <emsilver987@gmail.com>
2025-03-16 16:19:14 -0400

    Created a simple linear regression line with the data provided. Also can gather stats to confirm how good the estimate is based on our data reserved for testing: Mean absoulte error, mean squared erro, root mean squared erorr and r2 score

--- v1.py
+++ v1.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
 
 url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
 
@@ -10,27 +13,40 @@ print("Loading 5 Random Samples\n", df.sample(5))
 print("Loading Statstical Analysis of Data\n", df.describe())
 
 cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']] 
-# print("Selected a sample and features\n", cdf.sample(9))
 
-# viz = cdf[['CYLINDERS','ENGINESIZE','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
-# print("Visualize Histogram")
-# viz.hist()
-
-# print("Show plot")
-# plt.show()
-
-# plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
-# plt.xlabel("FUELCONSUMPTION_COMB")
-# plt.ylabel("Emission")
-# plt.show()
-
+# My Code for Cylinder against C02 Emissions
 plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color = 'orange')
 plt.xlabel("Cylinder")
 plt.ylabel("Emission")
 plt.show()
 
-# plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
-# plt.xlabel("Engine size")
-# plt.ylabel("Emission")
-# plt.xlim(0,27)
-# plt.show()
+X = cdf.ENGINESIZE.to_numpy()
+y = cdf.CO2EMISSIONS.to_numpy()
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+type(X_train), np.shape(X_train), np.shape(X_train)
+
+# create a model object
+regressor = linear_model.LinearRegression()
+
+# train the model on the training data
+# X_train is a 1-D array but sklearn models expect a 2D array as input for the training data, with shape (n_observations, n_features).
+# So we need to reshape it. We can let it infer the number of observations using '-1'.
+regressor.fit(X_train.reshape(-1, 1), y_train)
+
+# Print the coefficients
+print ('Coefficients: ', regressor.coef_[0]) # with simple linear regression there is only one coefficient, here we extract it from the 1 by 1 array.
+print ('Intercept: ',regressor.intercept_)
+
+plt.scatter(X_train, y_train,  color='blue')
+plt.plot(X_train, regressor.coef_ * X_train + regressor.intercept_, '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+# Use the predict method to make test predictions
+y_test_ = regressor.predict( X_test.reshape(-1,1))
+
+print("Mean absolute error: %.2f" % mean_absolute_error(y_test_, y_test))
+print("Mean squared error: %.2f" % mean_squared_error(y_test_, y_test))
+print("Root mean squared error: %.2f" % root_mean_squared_error(y_test_, y_test))
+print("R2-score: %.2f" % r2_score( y_test_, y_test) )

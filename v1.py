5adbb93af1c4208084063ea977a4c5b9b255e1c2
Ethan Silverthorne <emsilver987@gmail.com>
2025-03-23 21:27:59 -0400

    Exercise 1 and 2 - Adding new features to input and determing the log loss after graphing them

---
+++ v1.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.metrics import log_loss
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load in URL
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv"
+churn_df = pd.read_csv(url)
+churn_df
+
+# Declaring Fields
+churn_df = churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip', 'churn']]
+churn_df['churn'] = churn_df['churn'].astype('int')
+churn_df
+
+# Setting Input Fields and Target Fields 
+X = np.asarray(churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip']])
+X[0:5]  #print the first 5 values
+y = np.asarray(churn_df['churn'])
+y[0:5] #print the first 5 values
+
+# Normalize Data
+X_norm = StandardScaler().fit(X).transform(X)
+X_norm[0:5]
+
+# Split Training and Test Size
+X_train, X_test, y_train, y_test = train_test_split( X_norm, y, test_size=0.2, random_state=4)
+
+# SciKit-Learn Package
+LR = LogisticRegression().fit(X_train,y_train)
+
+# Predict Churn Variable from Training Set
+yhat = LR.predict(X_test)
+yhat[:10]
+
+# Predicition Probability - Threshold .5
+yhat_prob = LR.predict_proba(X_test)
+yhat_prob[:10]
+
+# Looking at how the input values affect our churn model
+coefficients = pd.Series(LR.coef_[0], index=churn_df.columns[:-1])
+coefficients.sort_values().plot(kind='barh')
+plt.title("Feature Coefficients in Logistic Regression Churn Model")
+plt.xlabel("Coefficient Value")
+plt.show()
+
+# Log Loss - Looking for low number
+log_loss(y_test, yhat_prob)
+
+
+# Exercise 1 - :et us assume we add the feature 'callcard' to the original set of input features. What will the value of log loss be in this case?
+url1 = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv"
+churn1_df = pd.read_csv(url1)
+churn1_df
+churn1_df = churn1_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip', 'callcard','churn']]
+churn1_df['churn'] = churn1_df['churn'].astype('int')
+churn1_df
+X1 = np.asarray(churn1_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip', 'callcard']])
+X1[0:5]
+y1 = np.asarray(churn1_df['churn'])
+y1[0:5]
+X1_norm = StandardScaler().fit(X1).transform(X1)
+X1_norm[0:5]
+X1_train, X1_test, y1_train, y1_test = train_test_split( X1_norm, y1, test_size=0.2, random_state=4)
+LR = LogisticRegression().fit(X1_train,y1_train)
+y1hat = LR.predict(X1_test)
+y1hat[:10]
+y1hat_prob = LR.predict_proba(X1_test)
+y1hat_prob[:10]
+coefficients = pd.Series(LR.coef_[0], index=churn1_df.columns[:-1])
+coefficients.sort_values().plot(kind='barh')
+plt.title("Feature Coefficients in Logistic Regression Churn Model")
+plt.xlabel("Coefficient Value")
+plt.show()
+print(log_loss(y1_test, y1hat_prob))
+
+
+# Exercise 2 - Let us assume we add the feature 'wireless' to the original set of input features. What will the value of log loss be in this case?
+url2 = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv"
+churn2_df = pd.read_csv(url2)
+churn2_df
+churn2_df = churn2_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip', 'wireless', 'churn']]
+churn2_df['churn'] = churn2_df['churn'].astype('int')
+churn2_df
+X2 = np.asarray(churn2_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'wireless', 'equip']])
+X2[0:5]
+y2 = np.asarray(churn2_df['churn'])
+y2[0:5]
+X2_norm = StandardScaler().fit(X2).transform(X2)
+X2_norm[0:5]
+X2_train, X2_test, y2_train, y2_test = train_test_split( X2_norm, y2, test_size=0.2, random_state=4)
+LR = LogisticRegression().fit(X2_train,y2_train)
+y2hat = LR.predict(X2_test)
+y2hat[:10]
+y2hat_prob = LR.predict_proba(X2_test)
+y2hat_prob[:10]
+coefficients = pd.Series(LR.coef_[0], index=churn2_df.columns[:-1])
+coefficients.sort_values().plot(kind='barh')
+plt.title("Feature Coefficients in Logistic Regression Churn Model")
+plt.xlabel("Coefficient Value")
+plt.show()
+print(log_loss(y2_test, y2hat_prob))

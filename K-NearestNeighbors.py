57036946490ff8b961bdf0837fcc52bba731012c
Ethan Silverthorne <emsilver987@gmail.com>
2025-03-26 10:08:27 -0400

    Exercise 1-5. Test for diff values of k and their affect on accurary. Explore reasons for that.

--- K-NearestNeighbors.py
+++ K-NearestNeighbors.py
@@ -39,3 +39,68 @@ knn_classifier = KNeighborsClassifier(n_neighbors=k)
 knn_model = knn_classifier.fit(X_train,y_train)
 yhat = knn_model.predict(X_test)
 print("Test set Accuracy: ", accuracy_score(y_test, yhat))
+
+# Choosing Correct Value of k
+Ks = 10
+acc = np.zeros((Ks))
+std_acc = np.zeros((Ks))
+for n in range(1,Ks+1):
+    #Train Model and Predict  
+    knn_model_n = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+    yhat = knn_model_n.predict(X_test)
+    acc[n-1] = accuracy_score(y_test, yhat)
+    std_acc[n-1] = np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+
+# Plot model accurary
+plt.plot(range(1,Ks+1),acc,'g')
+plt.fill_between(range(1,Ks+1),acc - 1 * std_acc,acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy value', 'Standard Deviation'))
+plt.ylabel('Model Accuracy')
+plt.xlabel('Number of Neighbors (K)')
+plt.tight_layout()
+plt.show()
+print( "The best accuracy was with", acc.max(), "with k =", acc.argmax()+1) 
+
+# Exercise 2 - Run the training model for 30 values of k and then again for 100 values of k. Identify the value of k that best suits this data and the accuracy on the test set for this model.
+Ks = 30
+acc = np.zeros((Ks))
+std_acc = np.zeros((Ks))
+for n in range(1,Ks+1):
+    #Train Model and Predict  
+    knn_model_n = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+    yhat = knn_model_n.predict(X_test)
+    acc[n-1] = accuracy_score(y_test, yhat)
+    std_acc[n-1] = np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+plt.plot(range(1,Ks+1),acc,'g')
+plt.fill_between(range(1,Ks+1),acc - 1 * std_acc,acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy value', 'Standard Deviation'))
+plt.ylabel('Model Accuracy')
+plt.xlabel('Number of Neighbors (K)')
+plt.tight_layout()
+plt.show()
+print( "The best accuracy was with", acc.max(), "with k =", acc.argmax()+1) 
+
+# Exercise 3 - Plot the variation of the accuracy score for the **training set** for 100 value of Ks.
+Ks =100
+acc = np.zeros((Ks-1))
+std_acc = np.zeros((Ks-1))
+for n in range(1,Ks):
+    #Train Model and Predict  
+    knn_model_n = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+    yhat = knn_model_n.predict(X_train)
+    acc[n-1] = accuracy_score(y_train, yhat)
+    std_acc[n-1] = np.std(yhat==y_train)/np.sqrt(yhat.shape[0])
+plt.plot(range(1,Ks),acc,'g')
+plt.fill_between(range(1,Ks),acc - 1 * std_acc, acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy value', 'Standard Deviation'))
+plt.ylabel('Model Accuracy')
+plt.xlabel('Number of Neighbors (K)')
+plt.tight_layout()
+plt.show()
+
+# Exercise 4 - Can you justify why the model performance on training data is deteriorating with increase in the value of k?
+# Anwser - K being too small means the model will be very sensetive to indiviaul points. Which may lead to overfitting. But, when k is large model start to account for more neighbors when making predictions
+# meaning the decision boundary becomes smoothers making the model less sensitive to noise. However, more generalize predicitions can lead to less perfect classifications. 
+
+# Exercise 5 - We can see that even the with the optimum values, the KNN model is not performing that well on the given data set. Can you think of the possible reasons for this?
+# Anwser - Boundraies could be unclear between classes just from raw data, weak correlation reduces discriminative power of KNN. Weak correlation also can itroduce noise and irrelevant variations.

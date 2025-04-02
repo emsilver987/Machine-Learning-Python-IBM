1e7c2e9afdecab6d2ce8f8615988b210884f90b3
Ethan Silverthorne <emsilver987@gmail.com>
2025-04-02 08:59:29 -0400

    Exercise - 1-5 - Expierment with different cluster sizes, plot

--- K-MeansClustering.py
+++ K-MeansClustering.py
@@ -34,3 +34,68 @@ ax.set_xticks(())
 ax.set_yticks(())
 plt.show()
 
+# Exercise 1 - Try to cluster the above dataset into a different number of clusters, say k=3. Note the difference in the pattern generated.
+k_means3 = KMeans(init="k-means++", n_clusters=3, n_init=12)
+k_means3.fit(X)
+fig = plt.figure(figsize=(6, 4))
+colors = plt.cm.tab10(np.linspace(0, 1, len(set(k_means3.labels_))))
+ax = fig.add_subplot(1, 1, 1)
+for k, col in zip(range(len(k_means3.cluster_centers_)), colors):
+    my_members = (k_means3.labels_ == k)
+    cluster_center = k_means3.cluster_centers_[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], 'w', markerfacecolor=col, marker='.',ms=10)
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,  markeredgecolor='k', markersize=6)
+plt.show()
+
+# Exercise 2 - Try the same with k=5.
+k_means3 = KMeans(init="k-means++", n_clusters=5, n_init=12)
+k_means3.fit(X)
+fig = plt.figure(figsize=(6, 4))
+colors = plt.cm.tab10(np.linspace(0, 1, len(set(k_means3.labels_))))
+ax = fig.add_subplot(1, 1, 1)
+for k, col in zip(range(len(k_means3.cluster_centers_)), colors):
+    my_members = (k_means3.labels_ == k)
+    cluster_center = k_means3.cluster_centers_[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], 'w', markerfacecolor=col, marker='.',ms=10)
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,  markeredgecolor='k', markersize=6)
+plt.show()
+
+# Exercise 3 - Comment on the within-cluster sum of squares, i.e. inertia, of the clusters created for k=3 and k=5. 
+# k =3 will give us a higher value of within-cluster sum of squarees than k=4. k=5 the value will be less than k=4.
+
+# Load Dataset
+cust_df = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%204/data/Cust_Segmentation.csv")
+
+# Pre-process
+cust_df = cust_df.drop('Address', axis=1)
+cust_df = cust_df.dropna()
+cust_df.info()
+
+# Normalize
+X = cust_df.values[:,1:] # leaves out `Customer ID`
+Clus_dataSet = StandardScaler().fit_transform(X)
+
+# Exercise 4 - Write the code to cluster the data with k=3. Extract the cluster labels for this clustering process.
+clusterNum = 3
+k_means3 = KMeans(init="k-means++", n_clusters=3, n_init=12)
+k_means.fit(X)
+labels = k_means .labels_
+
+# Plot - Age against income
+cust_df["Clus_km"] = labels
+cust_df.groupby('Clus_km').mean()
+area = np.pi * ( X[:, 1])**2  
+plt.scatter(X[:, 0], X[:, 3], s=area, c=labels.astype(float), cmap='tab10', ec='k',alpha=0.5)
+plt.xlabel('Age', fontsize=18)
+plt.ylabel('Income', fontsize=16)
+plt.show()
+
+# 3D Plot
+fig = px.scatter_3d(X, x=1, y=0, z=3, opacity=0.7, color=labels.astype(float))
+fig.update_traces(marker=dict(size=5, line=dict(width=.25)), showlegend=False)
+fig.update_layout(coloraxis_showscale=False, width=1000, height=800, scene=dict(
+        xaxis=dict(title='Education'),
+        yaxis=dict(title='Age'),
+        zaxis=dict(title='Income')
+    ))
+fig.show()

237d33862d6b348dcde9755323a944221b5286d6
Ethan Silverthorne <emsilver987@gmail.com>
2025-04-06 11:28:42 -0400

    import titanic data set and relevant features, drop unimportant features, exercise 1,2

---
+++ TitanicSurvivalPrediction.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, StratifiedKFold
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+import seaborn as sns
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+
+# Load Titanic and Select Relevant features
+titanic = sns.load_dataset('titanic')
+print(titanic.head())
+print(titanic.count())
+
+

5d97b5924f00bf9f5dadc20bdf2b5a5ece7c4a9f
Ethan Silverthorne <emsilver987@gmail.com>
2025-04-06 11:42:13 -0400

    make numerical and categorical columns, combine, and create model pipeline

--- TitanicSurvivalPrediction.py
+++ TitanicSurvivalPrediction.py
@@ -19,4 +19,43 @@ titanic = sns.load_dataset('titanic')
 print(titanic.head())
 print(titanic.count())
 
+# See which features we should drop
+features = ['pclass', 'sex', 'age', 'sibsp', 'parch', 'fare', 'class', 'who', 'adult_male', 'alone']
+target = 'survived'
+X = titanic[features]
+y = titanic[target]
 
+# Exercise 1. How balanced are the classes?
+print(y.value_counts())
+
+# Exercise 2. Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+# Detect Numerical and categorical columns 
+umerical_features = X_train.select_dtypes(include=['number']).columns.tolist()
+categorical_features = X_train.select_dtypes(include=['object', 'category']).columns.tolist()
+
+# Pipline for numerical transformer
+numerical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())
+])
+
+# Pipline for categorical transformer
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Combine transfomers into 1 preprocessor
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_features),
+        ('cat', categorical_transformer, categorical_features)
+    ])
+
+# Create Model Pipeline
+pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('classifier', RandomForestClassifier(random_state=42))
+])
